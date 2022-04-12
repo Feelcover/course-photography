af3a1bb1d6e9a319fd61d0d26dee1b53684e48cc
Andrey Chebanov <pioneerbeat@yandex.ru>
2022-04-12 18:01:02 +0300

    fixes after first review

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,66 @@
-const activeBurgerMenuButton = document.querySelector('.header__button_menu'); // Кнопка открытия меню
+const activeBurgerMenuButton = document.querySelector('.header__button-menu'); // Кнопка открытия меню
 const burgerMenu = document.querySelector('.header__burger'); //Выпадающее меню
-const closeBurgerMenuButton = document.querySelector('.header__button_close'); //Кнопка закрытия меню
 const mainMenu = document.querySelector('.header__main-menu'); // Основное меню
+const headerLogo = mainMenu.querySelector('.header__logo'); // Лого
+const headerLinks = mainMenu.querySelector('.header__links'); // Контейнер с ссылками
+const compositionLink = document.querySelector('#compositionLink'); // Ссылки из контейнера
+const cameraLink = document.querySelector('#cameraLink');
+const searchLink = document.querySelector('#searchLink');
+const closeBurgerMenuButton = mainMenu.querySelector('.header__button-close'); //Кнопка закрытия меню
 
 const submitButton = document.querySelector('.footer__button'); // Кнопка отправки формы
 const inputForm = document.querySelector('.footer__form'); // Форма
 
 const buttonMore = document.querySelector('.more'); //Кнопка "подробнее"
-const buttonEnroll = document.querySelector('.enroll'); //Кнопка "записаться"
-const buttonEnrollTwo = document.querySelector('.enroll-two'); //вторая кнопка "записаться"
+const buttonEnroll = document.querySelector('#enroll'); //Кнопка "записаться"
+const buttonEnrollTwo = document.querySelector('#enroll-two'); //вторая кнопка "записаться"
 
 //Открываем выпадающее меню
-activeBurgerMenuButton.addEventListener('click', function (event) {
-    mainMenu.classList.add('closed');
-    burgerMenu.classList.remove('closed');
-});
 
+activeBurgerMenuButton.addEventListener('click', function (event) {
+
+    mainMenu.classList.add('header__burger');
+    mainMenu.classList.remove('header__main-menu');
+
+    headerLogo.classList.add('header__burger-logo');
+    headerLogo.classList.remove('header__logo');
+
+    headerLinks.classList.add('header__burger-links');
+    headerLinks.classList.remove('header__links');
+
+    compositionLink.classList.add('header__burger-link');
+    compositionLink.classList.remove('header__link');
+    cameraLink.classList.add('header__burger-link');
+    cameraLink.classList.remove('header__link');
+    searchLink.classList.add('header__burger-link');
+    searchLink.classList.remove('header__link');
+    
+    activeBurgerMenuButton.classList.add('header__button-hidden');
+
+    closeBurgerMenuButton.classList.remove('header__button-hidden');
+});
 
 //Закрывыем выпадающее меню
 closeBurgerMenuButton.addEventListener('click', function (event) {
-    burgerMenu.classList.add('closed');
-    mainMenu.classList.remove('closed');
+    mainMenu.classList.remove('header__burger');
+    mainMenu.classList.add('header__main-menu');
+
+    headerLogo.classList.remove('header__burger-logo');
+    headerLogo.classList.add('header__logo');
+
+    headerLinks.classList.remove('header__burger-links');
+    headerLinks.classList.add('header__links');
+
+    compositionLink.classList.remove('header__burger-link');
+    compositionLink.classList.add('header__link');
+    cameraLink.classList.remove('header__burger-link');
+    cameraLink.classList.add('header__link');
+    searchLink.classList.remove('header__burger-link');
+    searchLink.classList.add('header__link');
+
+    activeBurgerMenuButton.classList.remove('header__button-hidden');
+
+    closeBurgerMenuButton.classList.add('header__button-hidden');
 });
 
 
